2c116c19013b59a37a7f8c4bcb2b63bb133c1947
longbiao_wu <615944323@qq.com>
2024-04-19 17:21:05 +0800

    feat: add types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-export const one = 1
-export const two = 2
+export type Expect<T extends true> = T
+export type ExpectTrue<T extends true> = T
+export type ExpectFalse<T extends false> = T
+export type IsTrue<T extends true> = T
+export type IsFalse<T extends false> = T
+
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+  ? true
+  : false
+export type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true
+
+// https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
+export type IsAny<T> = 0 extends 1 & T ? true : false
+export type NotAny<T> = true extends IsAny<T> ? false : true
+
+
+export type Pick<T, K extends keyof T> = {
+  [key in K]: T[key]
+}
+
+export type Readonly<T> = { readonly [P in keyof T]: T[P] }
+
+export type TupleToObject<T extends readonly (keyof any)[]> = {
+  [Key in T[number]]: Key
+}
+
+export type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R] ? F extends U ? true : Includes<R, U> : false
+
+export type Push<T extends any[], U> = [...T, U]
+export type Unshift<T extends any[], U> = [U, ...T]
